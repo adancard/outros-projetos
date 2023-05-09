5db30540fe72c79149788c3695b543f58d2ce508
Adan Cardoso <125130778+adancard@users.noreply.github.com>
2023-05-09 18:46:12 -0300

    Add files via upload

--- O lendario/java.js
+++ O lendario/java.js
@@ -2,7 +2,7 @@ var b = document.querySelector(".a2");
 b.addEventListener("mouseover",change);
 function change()
 {
-    var i = Math.floor(Math.random()*200)+1;
+    var i = Math.floor(Math.random()*300)+1;
     var j = Math.floor(Math.random()*250)+1;
     b.style.left = i+"px";
     b.style.top = j+"px";
